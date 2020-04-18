dcae14fa000eb7dffde504f3e7637e1362ccef6f
David Lyu <lyu.david@yahoo.com>
2020-04-17 23:33:30 -0700

    styled the header to look like wireframe

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Header(props) {
   return (
-    <>
+    <header>
       <h1>Student Grade Table</h1>
       <h3>Average Grades: <span className="badge badge-secondary">{props.average}</span></h3>
-    </>
+    </header>
   );
 }
